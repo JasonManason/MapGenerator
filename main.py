ac7bbe730e18847190855722b2263b6d6ddd0be0
JasonManason <Jasondemey1995@gmail.com>
2022-06-13 14:11:50 +0200

    some basic structures for pygame

--- main.py
+++ main.py
@@ -1,10 +1,60 @@
-import json
-
-file = open('nb_rules.json')
-data = json.load(file)
-
-for i in data["data"]:
-    print(i)
+import pygame, sys, json
+from pygame.locals import *
+import map, tile
 
 
-file.close()
+class MapGenerator:
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self.size = self.width, self.height = 400, 400
+        
+    def on_execute(self):
+        pass
+
+    def on_init(self):
+        pygame.init()
+        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self.FPS = 30
+        self.FramePerSec = pygame.time.Clock()
+        self._running = True
+ 
+    def on_event(self, event): #check if an event has occured
+        if event.type == pygame.QUIT:
+            self._running = False
+        
+        elif event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            self.start_collapse(pos)
+    
+    def start_collapse(self, pos): # mouse click will start collapse of tiles
+        print(pos)
+        # clicked_tile = [t for t in tiles if t.rect.collidepoint(pos)]
+        pass
+
+    def on_loop(self): # gameplay loop
+        pass
+
+    def on_render(self): # render to pygame window
+        pass
+
+    def on_cleanup(self): # close game
+        pygame.quit()
+        sys.exit()
+ 
+    def on_execute(self): # initialize pygame and enter main loop
+        if self.on_init() == False:
+            self._running = False
+ 
+        while( self._running ):
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render() 
+            self.FramePerSec.tick(self.FPS)
+        self.on_cleanup()
+
+
+if __name__ == "__main__":
+    MG = MapGenerator()
+    MG.on_execute()
---
+++ map.py
@@ -0,0 +1,4 @@
+class Map:
+    def __init__(self):
+        self.size = self.width, self.height = 320, 320 # something that fits in eventual window and is some multiple of 16x16 from the tiles
+        
