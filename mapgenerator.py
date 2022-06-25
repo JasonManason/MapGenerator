1564b55f2f90aa6f3c3d0b1c9a4b3aaf6edc0f5c
JasonManason <Jasondemey1995@gmail.com>
2022-06-25 14:34:47 +0200

    complete overhaul, entropy works, just need adjacency check occupied tiles and backtracking

---
+++ mapgenerator.py
@@ -0,0 +1,238 @@
+import pygame, sys, random, tile, json
+from pygame.locals import *
+from itertools import product
+import numpy as np
+IMG_SIZE = 16
+
+class MapGenerator:
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self.size = self.width, self.height = 320, 320
+        self.occupied_tiles = []
+
+
+    def on_init(self):
+        pygame.init()
+        self.display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self.FPS = 30
+        self.FramePerSec = pygame.time.Clock()
+        self.running = True
+
+
+    def on_event(self, event: pygame.event):
+        if event.type == pygame.QUIT:
+            self.running = False
+        
+        elif event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            self.initialize_wfc(pos)
+            pygame.event.set_blocked(pygame.MOUSEBUTTONUP)
+
+
+    def draw_tile(self, t: tile):
+        self.display_surf.blit((pygame.image.load(f"tiles/{t.img_name}.png")), t.coords)
+        pygame.display.update()
+       
+
+    def load_data(self):
+        file = open('nb_rules.json')
+        data = json.load(file)
+        file.close()
+        return data["data"], [i for i in data["data"]]
+
+
+    def set_pos_first_tile(self, t:tile, pos:tuple):
+        """
+            Sets the coordinates of the first tile to the correct position on the grid.
+        """
+        coords = [c for c in range(self.width) if c % 16 == 0]
+        coordinates = list(product(coords, coords))
+
+        for x_y in coordinates:
+            rect_x_y = pygame.Rect(x_y, (16, 16))
+            if rect_x_y.collidepoint(pos):
+                t.set_coords(x_y)
+                self.occupied_tiles.append(x_y)
+
+
+    def get_valid_nbs(self, name: str, data: dict) -> list:
+        """
+            Returns a nested list of all valid neighbours of a given tile.
+        """
+        up, down, left, right = [], [], [], []
+        for i in data:
+            if i == name:
+                up, down, left, right = list(data[i].values())
+
+        return up, down, left, right
+
+
+    def create_grid(self, img_names:list):
+        """
+            Creates a grid and fills every position with all possible tiles.
+        """
+        self.grid = np.ndarray((int(self.width / IMG_SIZE), int(self.height / IMG_SIZE)), dtype=list)
+        self.grid.fill(img_names)
+
+
+    def update_grid(self, t:tile) -> tuple:
+        """
+            Changes the spot on the grid to the drawn tile and returns the indexes of the grid as a tuple.
+        """
+        x, y = t.coords
+        step = IMG_SIZE
+        if x != 0 and y != 0:
+            self.grid[int(x / step)][int(y / step)] = [t.img_name]
+            return (int(x / step), int(y / step))
+        elif x == 0 and y != 0:
+            self.grid[0][int(y / step)] = [t.img_name]
+            return (0, int(y / step))
+        elif y == 0:
+            self.grid[int(x / step)][0] = [t.img_name]
+            return (int(x / step), 0)
+        else:
+            self.grid[0][0] = [t.img_name]
+            return (0, 0)
+
+
+    def update_grid_around_tile(self, t:tile, x: int, y: int):
+        """
+            Changes the possible outcomes of tiles around the drawn tile.
+        """
+        step = 1
+        UP, DOWN, LEFT, RIGHT = (x, y - step), (x, y + step), (x - step, y), (x + step, y)
+        max = int((self.width / IMG_SIZE) - 1)
+
+        if x == 0: # LEFT BORDER
+            if y == 0: # CORNER L/U                    
+                if len(self.grid[DOWN]) != 1: self.grid[DOWN] = t.valid_nbs_down
+                if len(self.grid[RIGHT]) != 1: self.grid[RIGHT] = t.valid_nbs_right
+            elif y == max: # CORNER L/D
+                if len(self.grid[UP]) != 1: self.grid[UP] = t.valid_nbs_up    
+                if len(self.grid[RIGHT]) != 1: self.grid[RIGHT] = t.valid_nbs_right
+            else:
+                if len(self.grid[UP]) != 1: self.grid[UP] = t.valid_nbs_up
+                if len(self.grid[DOWN]) != 1: self.grid[DOWN] = t.valid_nbs_down
+                if len(self.grid[RIGHT]) != 1: self.grid[RIGHT] = t.valid_nbs_right
+
+        elif x == max: # RIGHT BORDER     
+            if y == 0: # CORNER R/U
+                if len(self.grid[DOWN]) != 1: self.grid[DOWN] = t.valid_nbs_down
+                if len(self.grid[LEFT]) != 1: self.grid[LEFT] = t.valid_nbs_left
+            elif y == max: # CORNER R/D
+                if len(self.grid[UP]) != 1: self.grid[UP] = t.valid_nbs_up
+                if len(self.grid[LEFT]) != 1: self.grid[LEFT] = t.valid_nbs_left
+            else:
+                if len(self.grid[UP]) != 1: self.grid[UP] = t.valid_nbs_up
+                if len(self.grid[DOWN]) != 1: self.grid[DOWN] = t.valid_nbs_down
+                if len(self.grid[LEFT]) != 1: self.grid[LEFT] = t.valid_nbs_left
+
+        elif y == 0 and x != 0 and x != max: # UPPER BORDER
+            if len(self.grid[DOWN]) != 1: self.grid[DOWN] = t.valid_nbs_down
+            if len(self.grid[LEFT]) != 1: self.grid[LEFT] = t.valid_nbs_left
+            if len(self.grid[RIGHT]) != 1: self.grid[RIGHT] = t.valid_nbs_right
+        elif y == max and x != 0 and x != max: # BOTTOM BORDER
+            if len(self.grid[UP]) != 1: self.grid[UP] = t.valid_nbs_up
+            if len(self.grid[LEFT]) != 1: self.grid[LEFT] = t.valid_nbs_left
+            if len(self.grid[RIGHT]) != 1: self.grid[RIGHT] = t.valid_nbs_right
+
+        else: # ALL SIDES FREE
+            if len(self.grid[UP]) != 1: self.grid[UP] = t.valid_nbs_up
+            if len(self.grid[DOWN]) != 1: self.grid[DOWN] = t.valid_nbs_down
+            if len(self.grid[LEFT]) != 1: self.grid[LEFT] = t.valid_nbs_left
+            if len(self.grid[RIGHT]) != 1: self.grid[RIGHT] = t.valid_nbs_right
+
+
+
+    def get_min_entropy(self) -> tuple:
+        """
+            Looks for the cells in the grid with the lowest entropy and returns the coordinates with the possible tiles.
+        """
+        options, coords = [], []
+
+        for i, row in enumerate(self.grid):
+            for j, elem in enumerate(row):
+                if len(elem) != 1 and len(elem) != 36: # 1=fully collapsed and 36=not adjacent to anything
+                    options.append(elem)
+                    coords.append((i, j))
+
+        next = random.choice(options)
+        index = options.index(next)
+        self.grid[i][j] = next
+        return next, coords[index]
+
+
+    def is_fully_collapsed(self) -> bool:
+        """
+            Checks if the wfc is fully collapsed by checking if any of the cells in the grid have more than 1 options.
+        """
+        for row in self.grid:
+            for elem in row:
+                if(len(elem) > 1):
+                    return False
+        return True
+
+
+    def initialize_wfc(self, pos):
+        """
+            wfc = wave function collapse
+        """
+        data, img_names = self.load_data()
+        name = random.choice(img_names)
+        self.get_valid_nbs(name, data)
+        nbs_up, nbs_down, nbs_left, nbs_right = self.get_valid_nbs(name, data)
+        first_tile = tile.Tile(name, IMG_SIZE, IMG_SIZE, nbs_up, nbs_down, nbs_left, nbs_right)
+        self.set_pos_first_tile(first_tile, pos)
+        self.draw_tile(first_tile)
+        self.create_grid(img_names)
+        x, y = self.update_grid(first_tile)
+        self.update_grid_around_tile(first_tile, x, y)
+        self.wave_function_collapse(data)
+
+
+    def wave_function_collapse(self, data: dict):
+        """
+            Checks if not fully collapsed and then picks a new position based on the lowest entropy (least amount of tile options).
+            Then a new tile object gets created and drawn in that spot.
+            The grid gets updated and the process repeats until the wave is fully collapsed (the len of all options in the grid is equal to 1).
+        """
+        while not self.is_fully_collapsed():
+            options, (x, y) = self.get_min_entropy()
+            if (x, y) not in self.occupied_tiles:
+                name = random.choice(options)
+                up, down, left, right = self.get_valid_nbs(name, data)
+                next = tile.Tile(name, IMG_SIZE, IMG_SIZE, up, down, left, right)
+                next.set_coords((int(x * 16), int(y * 16)))
+                self.occupied_tiles.append((x, y))
+                self.draw_tile(next)
+                self.update_grid(next)
+                self.update_grid_around_tile(next, x, y)
+
+        
+    def on_startup(self):
+        # EXTRA: draw a blended image of all options for every spot in grid?
+        pass
+    
+
+    def update_blend(self):
+        # EXTRA: change the blended image to options left for every spot in grid?
+        pass
+
+
+    def on_cleanup(self):
+        pygame.quit()
+        sys.exit()
+ 
+
+    def on_execute(self):
+        if self.on_init() == False:
+            self.running = False
+        
+        pygame.display.set_caption('MapGenerator')
+        self.on_startup()
+        while(self.running ):
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.FramePerSec.tick(self.FPS)
+        self.on_cleanup()
--- tile.py
+++ tile.py
@@ -1,69 +1,32 @@
-from numpy import tile
-import pygame, json, mapoftiles, random
+import pygame
 
-class Tile(pygame.sprite.Sprite): # https://www.pygame.org/docs/ref/sprite.html#pygame.sprite.Sprite
-    def __init__(self):
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, img_name, width, height, valid_nbs_up, valid_nbs_down, valid_nbs_left, valid_nbs_right): #, weight):
         super().__init__()
+        self.img_name = img_name
+        self.width = width
+        self.height = height
+        self.valid_nbs_up = valid_nbs_up
+        self.valid_nbs_down = valid_nbs_down
+        self.valid_nbs_left = valid_nbs_left
+        self.valid_nbs_right = valid_nbs_right
+        #self.weight = weight
 
 
-    def set_initial_nbs(self):
-        file = open('nb_rules.json')
-        data = json.load(file)
-        self.nbs = data["data"]
-        file.close()
+    def set_img_name(self, img_name: str):
+        self.img_name = img_name
 
 
-    def set_nbs(self, nbs: dict):
-        self.nbs = nbs
+    def get_img_name(self) -> str:
+        return self.img_name
 
 
-    def get_nbs(self) -> dict:
+    def set_nbs(self):
+        self.nbs = [self.valid_nbs_up, self.valid_nbs_down, self.valid_nbs_left, self.valid_nbs_right]
+
+
+    def get_nbs(self) -> list:
         return self.nbs
-    
-
-    def set_name(self, name: str):
-        self.name = name
-    
-
-    def get_name(self) -> str:
-        return self.name
-
-
-    def check_adjacency(self, coords: tuple, m: mapoftiles) -> tuple(list, dict):
-        """
-        Returns only the coordinates of the free adjacent tiles.
-        """
-        nbs = self.get_nbs()
-        coords_up = (coords[0], coords[1] - 16)
-        coords_down = (coords[0], coords[1] + 16)
-        coords_left = (coords[0] - 16, coords[1])
-        coords_right = (coords[0] + 16, coords[1])
-        
-        #Check vertical sides
-        if coords[0] == 0:
-            if coords[1] == 0: # CORNER LEFT UP
-                return [coords_down, coords_right], {n: nbs[self.name][n] for n in nbs[self.name].keys() & ["nb_down", "nb_right"]}
-            elif coords[1] == (m.height - 16): # CORNER LEFT DOWN
-                return [coords_up, coords_right], {n: nbs[self.name][n] for n in nbs[self.name].keys() & ["nb_up", "nb_right"]}
-            else: # no left nbs
-                return [coords_up, coords_down, coords_right], {n: nbs[self.name][n] for n in nbs[self.name].keys() & ["nb_up", "nb_down", "nb_right"]}
-
-        elif coords[0] == (m.width - 16):
-            if coords[1] == 0: # CORNER RIGHT UP
-                return [coords_down, coords_left], {n: nbs[self.name][n] for n in nbs[self.name].keys() & ["nb_down", "nb_left"]}
-            elif coords[1] == (m.height - 16): # CORNER RIGHT DOWN
-                return [coords_up, coords_left], {n: nbs[self.name][n] for n in nbs[self.name].keys() & ["nb_up", "nb_left"]}
-            else: # no right nbs
-                return [coords_up, coords_down, coords_left], {n: nbs[self.name][n] for n in nbs[self.name].keys() & ["nb_up", "nb_down", "nb_left"]}
-
-        # Check horizontal sides
-        elif coords[1] == 0: # no up nbs
-            return [coords_down, coords_left, coords_right], {n: nbs[self.name][n] for n in nbs[self.name].keys() & ["nb_down", "nb_left", "nb_right"]}
-        elif coords[1] == (m.height - 16): # no down nbs
-            return [coords_up, coords_left, coords_right], {n: nbs[self.name][n] for n in nbs[self.name].keys() & ["nb_up", "nb_left", "nb_right"]}
-              
-        else: # all sides are free
-            return [coords_up, coords_down, coords_left, coords_right], nbs[self.name]
 
 
     def set_coords(self, coords: tuple):
